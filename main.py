0563e0ace43bb97f455df44dfeca288dba488877
cc0088 <41247002s@gapps.ntnu.edu.tw>
2025-11-19 21:41:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -53,19 +53,19 @@ def main(dataset='Kaggle2014Pred', build_type='cv'):
         ]
     else:
         targets = [
-             '1',
-            # '2',
-            # '3',
-            # '5',
-            # '9',
-            # '10',
-            # '13',
-            # '14',
-            # '18',
-            # '19',
+            #'1',
+             '2',
+             '3',
+             '5',
+             '9',
+             '10',
+             '13',
+             '14',
+             '18',
+             '19',
             #'20',
-            #'21',
-            #'23'
+            '21',
+            '23'
         ]
 
     for target in targets:
@@ -137,6 +137,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     assert args.mode in ['cv','test']
     main(dataset=args.dataset, build_type=args.mode)
+
 
 
 
